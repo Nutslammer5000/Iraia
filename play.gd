24152f0330da084545487508b4ed2c2f04397796
Nutslammer5000 <nicros76@kingshigh.school.nz>
2024-09-19 12:06:58 +1200

    1.1
    i added an end and finished the second level

--- play.gd
+++ play.gd
@@ -9,3 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file("res://END SCREEN.tscn")
